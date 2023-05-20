d2ca4e670d903e32a00701766ebf4784f3b8c7ec
bennytrt <super.b.sb15@gmail.com>
2023-05-20 05:23:38 +0200

    erstt

--- Classes/FindElement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace Chronos.Classes
-{
-    internal static class FindElement
-    {
-        internal static T FindElementChild<T>(this Control Element, string Child) => Element.Template.FindName(Child, Element) is T x ? x : default(T);
-    }
-}
